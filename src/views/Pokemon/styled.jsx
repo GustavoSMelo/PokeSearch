f699a93781fa410858226a941cde130640dac92b
Gustavo Santos Melo <gsantos15569@gmail.com>
2019-12-18 23:34:08 -0300

    modificação de estilo do app

--- src/views/Pokemon/styled.jsx
+++ src/views/Pokemon/styled.jsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.ScrollView``;
+export const Container = styled.View`
+    margin-top: 15px;
+    margin-bottom: 8px;
+`;
 
 export const FigureView = styled.View`
     flex: 1;
@@ -19,12 +22,151 @@ export const MarginView = styled.View`
     font-size: 18px;
 `;
 
-export const ListItem = styled.View`
-    margin-top: 7px;
-    margin-bottom: 7px;
+export const ListItem = styled.Text`
+    margin-top: 10px;
+    margin-bottom: 10px;
+    font-size: 20px;
 `;
 
 export const Bold = styled.Text`
     font-weight: bold;
     font-size: 24px;
+    text-align: center;
+`;
+
+export const TypeText = styled.Text`
+    text-align: center;
+`;
+
+export const ContainerText = styled.View`
+    width: 80px;
+    height: 30px;
+    border-radius: 30px;
+    padding: 5px;
+`;
+
+export const ContainerTitle = styled.View`
+    margin-top: 45px;
+    margin-bottom: 10px;
+`;
+
+export const TitleText = styled.Text`
+    font-weight: bold;
+    text-align: center;
+`;
+
+export const TypeSteel = styled.View`
+    background-color: #c0c2f5;
+    color: #fff;
+    padding: 4px;
+`;
+
+export const TypeFire = styled.View`
+    background-color: #ff831c;
+    padding: 4px;
+`;
+
+export const TypeIce = styled.View`
+    background-color: #9fe9da;
+    padding: 4px;
+`;
+
+export const TypeWater = styled.View`
+    background-color: #005ff0;
+    padding: 4px;
+`;
+
+export const TypeGrass = styled.View`
+    background-color: #00b828;
+    padding: 4px;
+`;
+
+export const TypeGround = styled.View`
+    background-color: #f0c988;
+    padding: 4px;
+`;
+
+export const TypeGhost = styled.View`
+    background-color: #6e56f0;
+    padding: 4px;
+`;
+
+export const TypeRock = styled.View`
+    background-color: #f08872;
+    padding: 4px;
+`;
+
+export const TypeFairy = styled.View`
+    background-color: #e9abf0;
+    padding: 4px;
+`;
+
+export const TypeFight = styled.View`
+    background-color: #f03100;
+    padding: 4px;
+    color: #fff;
+`;
+
+export const TypeFly = styled.View`
+    background-color: #00d4f0;
+    padding: 4px;
+`;
+
+export const TypeElectric = styled.View`
+    background-color: #f0e357;
+    padding: 4px;
+`;
+
+export const TypeDark = styled.View`
+    background-color: #383434;
+    padding: 4px;
+    color: #fff;
+`;
+
+export const TypePoison = styled.View`
+    background-color: #8800d1;
+    padding: 4px;
+`;
+
+export const TypeNormal = styled.View`
+    background-color: #999999;
+    padding: 4px;
+`;
+
+export const TypeBug = styled.View`
+    background-color: #91c26c;
+
+    padding: 4px;
+`;
+
+export const TypeDragon = styled.View`
+    background-color: #00005c;
+    padding: 4px;
+`;
+
+export const TypePsych = styled.View`
+    background-color: #eb4cc6;
+    text-align: center;
+    padding: 4px;
+`;
+
+export const Name = styled.View`
+    background-color: #c0c2f5;
+    color: #000;
+    margin-bottom: 15px;
+    padding-top: 45px;
+    padding-bottom: 10px;
+    align-items: center;
+`;
+
+export const TypeContainer = styled.View`
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: row;
+`;
+
+export const StrongTitle = styled.Text`
+    font-weight: bold;
+    font-size: 24px;
+    padding: 15px;
 `;
